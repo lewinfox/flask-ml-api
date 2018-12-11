4e66e0ad68c6dacd92aa58bbd7e2abc960725ecf
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2018-12-11 20:19:45 +0000

    Move data processing into the model package

--- iris/routes.py
+++ iris/routes.py
@@ -8,19 +8,30 @@ from model.model_api import make_prediction
 @app.route("/")
 @app.route("/index")
 def index():
+    """Serve the application's main page."""
+
     return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    """Obtain a prediction from the model.
+
+    The intention is that this function should need little to no modification
+    to work with different models. Its only purpose is to collect the
+    data from the POST request, pass it to the model's `make_prediction`
+    function, and return the output of that function to the client. All
+    data processing should be handled by `model.make_prediction`.
+
+    Note that it returns the data in JSON format - is it assumed that we are
+    working with AJAX requests. If the client is not expecting an AJAX response
+    the raw JSON should still be displayed.
+
+    What the browser does with the response is also not specified here - write
+    your own JavaScript function at the other end.
+    """
     try:
-        data = {
-            "sepal_length": request.form.get("sepal-length"),
-            "sepal_width": request.form.get("sepal-width"),
-            "petal_length": request.form.get("petal-length"),
-            "petal_width": request.form.get("petal-width")
-        }
-        input_data = [x for x in data.values()]
-        result = make_prediction(model, input_data)
+        data = request.form
+        result = make_prediction(model, data)
         return jsonify(result)
     except Exception as e:
-        print(e)
+        print(e)  # TODO: Make this nicer
--- model/model_api.py
+++ model/model_api.py
@@ -1,10 +1,35 @@
+# -*- coding: utf-8 -*-
+"""API for interacting with sklean model.
+
+Contains a function for obtaining a prediction in the desired format from a
+predetermined sklearn iris classification model.
+"""
+
 import numpy as np
 
 
 def make_prediction(model, data):
-    """Predict class from input data"""
+    """Predict class from input data
 
-    data = np.array(data)
-    species = model.predict([data])[0]
-    pred_prob = max(model.predict_proba([data])[0])
+    Calls `model.predict()` on `data`.
+
+    Args:
+        model (model): A pretrained sklearn model.
+        data (list): A list of parameters to be passed to `model`.
+
+    Returns:
+        dict: A dictionary containing the predicted species (`pred_class`) and
+        prediction confidence (`pred_prob`).
+    """
+
+    data_dict = {
+        "sepal_length": data.get("sepal-length"),
+        "sepal_width": data.get("sepal-width"),
+        "petal_length": data.get("petal-length"),
+        "petal_width": data.get("petal-width")
+    }
+    input_data = [x for x in data_dict.values()]
+    data = np.array(input_data)
+    species = model.predict([input_data])[0]
+    pred_prob = max(model.predict_proba([input_data])[0])
     return {"pred_class": species, "pred_prob": pred_prob}
